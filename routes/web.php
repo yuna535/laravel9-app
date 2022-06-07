e0299664a2fadd0524952da1d9685a3313815d46
yuna535 <58472579+yuna535@users.noreply.github.com>
2022-06-08 08:13:51 +0900

    add route,controller,view

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sample', [\App\Http\Controllers\Sample\IndexController::class,'show']);
+Route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class,'showId']);
+
+Route::get('/tweet', [\App\Http\Controllers\Tweet\IndexController::class,'__invoke']);
+Route::get('/tweet/{id}', [\App\Http\Controllers\Tweet\IndexController::class,'invokeId']);
