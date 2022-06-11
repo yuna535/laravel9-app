a2e603b9be73974700ed89279182f161d3574a44
yuna535 <58472579+yuna535@users.noreply.github.com>
2022-06-11 17:47:57 +0900

    編集画面を作成

---
+++ app/Http/Controllers/Tweet/Update/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Tweet\Update;
+
+use App\Http\Controllers\Controller;
+use App\Models\Tweet;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class IndexController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $tweetId = (int) $request->route('tweetId');
+        $tweet = Tweet::where('id', $tweetId)->firstOrFail();
+        return view('tweet.update')->with('tweet', $tweet);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,10 @@ Route::get('/', function () {
 Route::get('/sample', [\App\Http\Controllers\Sample\IndexController::class,'show']);
 Route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class,'showId']);
 
-Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)->name('tweet.index');
+Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)
+    ->name('tweet.index');
+Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)
+    ->name('tweet.create');
 
-Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)->name('tweet.create');
+Route::get('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexController::class)->name('tweet.update.index');
+Route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)->name('tweet.update.put');
