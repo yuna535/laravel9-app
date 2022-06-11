e50b3c1f248f6a4e86efd3c4c49c22ca4a1a596c
yuna535 <58472579+yuna535@users.noreply.github.com>
2022-06-11 17:14:23 +0900

    投稿をデータベースに保存する

--- app/Http/Controllers/Tweet/CreateController.php
+++ app/Http/Controllers/Tweet/CreateController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Tweet;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\Tweet\CreateRequest;
+use App\Models\Tweet;
 
 class CreateController extends Controller
 {
@@ -16,6 +17,9 @@ class CreateController extends Controller
      */
     public function __invoke(CreateRequest $request)
     {
-        //
+        $tweet = new Tweet();
+        $tweet->content = $request->tweet();
+        $tweet->save();
+        return redirect()->route('tweet.index');
     }
 }
