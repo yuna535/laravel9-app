4a68ef52f3373d3abdb88e2eb7cabf19c9d1f70d
yuna535 <58472579+yuna535@users.noreply.github.com>
2022-06-15 08:45:34 +0900

    ログイン機能追加

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+// sample
 Route::get('/sample', [\App\Http\Controllers\Sample\IndexController::class,'show']);
 Route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class,'showId']);
 
+// tweet
 Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)
     ->name('tweet.index');
 Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)
     ->name('tweet.create');
-
 Route::get('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexController::class)->name('tweet.update.index');
 Route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)->name('tweet.update.put');
-
 Route::delete('/tweet/delete/{tweetId}', \App\Http\Controllers\Tweet\DeleteController::class)->name('tweet.delete');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
