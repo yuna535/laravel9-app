10040dfb26c52ee91c0c2b4b546f3fb16a2897f5
yuna535 <58472579+yuna535@users.noreply.github.com>
2022-06-15 08:29:16 +0900

    DI

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Tweet;
+use App\Services\TweetService;
 
 class IndexController extends Controller
 {
@@ -14,9 +14,9 @@ class IndexController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request,TweetService $tweetService)
     {
-        $tweets = Tweet::orderBy('created_at', 'DESC')->get();
+        $tweets = $tweetService->getTweets();
         // dd($tweets);
         return view('tweet.index')->with('tweets', $tweets);
     }
