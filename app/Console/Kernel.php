3a2efee10b1d0070ce847c312df317773f67f804
yuna535 <58472579+yuna535@users.noreply.github.com>
2022-06-20 22:42:20 +0900

    sample scheduler を作成

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,18 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        // // 毎分
+        $schedule->command('sample-command')->everyMinute()->emailOutputTo('info@example.com');
+        // // 毎時
+        // $schedule->command('sample-command')->hourly();
+        // // 毎時8分
+        // $schedule->command('sample-command')->hourlyAt(8);
+        // // 毎日
+        // $schedule->command('sample-command')->daily();
+        // // 毎日13時
+        // $schedule->command('sample-command')->dailyAt('13:00');
+        // // 毎日3:15 cron表記
+        // $schedule->command('sample-command')->cron('15 3 * * *');
     }
 
     /**
