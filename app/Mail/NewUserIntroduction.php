051daca40e72da5c706c28b372cecb76f1eead04
yuna535 <58472579+yuna535@users.noreply.github.com>
2022-06-20 21:59:17 +0900

    job queue を実装

--- app/Mail/NewUserIntroduction.php
+++ app/Mail/NewUserIntroduction.php
@@ -8,7 +8,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use App\Models\User;
 
-class NewUserIntroduction extends Mailable
+class NewUserIntroduction extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
