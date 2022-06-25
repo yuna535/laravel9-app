eabaa755d3acbfdea3bdc3e906f3c0a2f9109c56
yuna535 <58472579+yuna535@users.noreply.github.com>
2022-06-25 14:22:51 +0900

    unitテスト作成

---
+++ tests/Unit/Services/TweetServiceTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use App\Services\TweetService;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+
+class TweetServiceTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     * @runInSeparateProcess
+     * @return void
+     */
+    public function test_check_own_tweet()
+    {
+        $tweetService = new TweetService();
+
+        $mock = Mockery::mock('alias:App\Models\Tweet');
+        $mock->shouldReceive('where->first')->andReturn((object)[
+            'id' => 1,
+            'user_id' => 1
+        ]);
+
+
+
+        $result = $tweetService->checkOwnTweet(1,1);
+        $this->assertTrue($result);
+
+        $result = $tweetService->checkOwnTweet(2,1);
+        $this->assertFalse($result);
+    }
+}
